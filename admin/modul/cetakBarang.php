a483bab61918b5fe8769e563ad2e30c1838e791f
Ilzam Afif <ilzamafif27.com>
2022-03-22 08:34:47 +0700

    tambah library

--- admin/modul/cetakBarang.php
+++ admin/modul/cetakBarang.php
@@ -6,14 +6,14 @@ include '../../config/db.php';
 
 <head>
   <title>Data Barang</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+  <link rel="stylesheet" href="../../assets/library/bootstrap.min.css">
+  <script src="../../assets/library/"></script>
+  <script src="../../assets/library/popper.min.js"></script>
+  <script src="../../assets/library/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="../../assets/library/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/library/buttons.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/library/jquery.dataTables.min.css">
+  <script type="text/javascript" charset="utf8" src="../../assets/library/jquery.dataTables.js"></script>
 </head>
 
 <body>
@@ -58,15 +58,15 @@ include '../../config/db.php';
     });
   </script>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+  <script src="../../assets/library/jquery-3.5.1.js"></script>
+  <script src="../../assets/library/jquery.dataTables.min.js"></script>
+  <script src="../../assets/library/dataTables.buttons.min.js"></script>
+  <script src="../../assets/library/buttons.flash.min.js"></script>
+  <script src="../../assets/library/jszip.min.js"></script>
+  <script src="../../assets/library/pdfmake.min.js"></script>
+  <script src="../../assets/library/vfs_fonts.js"></script>
+  <script src="../../assets/library/buttons.html5.min.js"></script>
+  <script src="../../assets/library/buttons.print.min.js"></script>
 
 
 
